90412865949acbe3d80ea2efac7ea21d93a734d0
Jiahao Ye <jiahao.ye@enti.cat>
2025-01-21 20:09:42 +0100

    WS implementation

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 
 const PORT = 7777;
 
+let http = require('http');
 let static = require('node-static');
- 
+let ws = require('ws'); 
 //
 // Create a node-static server instance to serve the './public' folder
 //
 let file = new static.Server('./public');
  
-require('http').createServer(function (request, response) {
+let http_server = http.createServer(function (request, response) {
     request.addListener('end', function () {
         //
         // Serve files!
@@ -16,3 +17,63 @@ require('http').createServer(function (request, response) {
         file.serve(request, response);
     }).resume();
 }).listen(PORT);
+
+
+let ws_server = new ws.Server({server: http_server});
+let player1, player2;
+let ball_pos;
+ws_server.on('connection', function (conn) {
+
+	console.log("Usuario conectado");
+
+	if (player1 == null){
+		player1 = conn;
+
+		let info = {
+			player_num: 1
+		}
+
+		player1.send( JSON.stringify(info));
+		player1.on('message', function (msg) {
+			
+			if(player2 == null)
+				return;
+
+			let info = JSON.parse(msg);
+
+			if (info.y != null){
+				player2.send( JSON.stringify(info));
+
+			}
+			else if (info.by != null){
+				player2.send( JSON.stringify(info));
+			}
+			else if (info.s1 != null){
+				player2.send(JSON.stringify(info));
+			}
+		});
+	}
+	else if (player2 == null){
+		player2 = conn;
+
+		let info = {
+			player_num: 2
+		}
+
+		player2.send( JSON.stringify(info));
+
+		player2.on('message', function (msg) {
+			if (player1 == null)
+				return;
+			
+			let info = JSON.parse(msg);
+
+			if (info.y != null){
+				player1.send( JSON.stringify(info));
+			}
+		});
+	}
+
+	
+});
+
